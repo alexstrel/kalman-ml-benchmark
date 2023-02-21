97777b6fd87301b7814cae682259371afb8d2dcf
Strelchenko Alexei <astrel@fnal.gov>
2023-02-21 00:12:26 -0600

    massive cleaning

--- kalman-algo-test.cpp
+++ kalman-algo-test.cpp
@@ -51,6 +51,33 @@ using Float    = float;
 using ReduceTp = Float;
 #endif
 
+// Simple arithmetic type
+template <typename T>
+concept FloatTp = requires{
+  requires std::is_floating_point_v<T>;
+};
+
+// Simple complex type
+template <typename T>
+concept ComplexTp    = requires (T t) {
+  requires FloatTp<decltype(t.real())>;
+  requires FloatTp<decltype(t.imag())>;  
+};
+
+// Simple genetic arithmetic type
+template <typename T>
+concept ArithmeticTp = FloatTp<T> || ComplexTp<T>;
+
+
+// Generic container type:
+template <typename T>
+concept GenericContainerTp  = requires (T t) {
+  t.begin();
+  t.end();
+  t.data();
+  t.size();
+};
+
 constexpr int nobs     = NOBS;
 constexpr int fc_steps = FCSTEPS;
 
@@ -59,6 +86,7 @@ constexpr int D     = 8;
 
 using bFloatN  = std::array<Float, bSize*D >;
 using bFloatNN = std::array<Float, bSize*D*D>;
+using Float1   = std::array<Float, 1>;
 
 //
 using indx_type     = int; 
@@ -71,26 +99,8 @@ using Left2DCView     =  stdex::mdspan<const Float, stdex::extents<indx_type, D,
 using Right2DView     =  stdex::mdspan<Float, stdex::extents<indx_type, D, D>, stdex::layout_right, stdex::default_accessor<Float>>;
 using Right2DCView    =  stdex::mdspan<const Float, stdex::extents<indx_type, D, D>, stdex::layout_right, stdex::default_accessor<const Float>>;
 
-using Strided2DView   =  stdex::mdspan<Float, stdex::extents<indx_type, D, D>, stdex::layout_stride, stdex::default_accessor<Float>>;
-using Strided2DCView  =  stdex::mdspan<const Float, stdex::extents<indx_type, D, D>, stdex::layout_stride, stdex::default_accessor<const Float>>;
-
 using Left1DView      =  stdex::mdspan<Float, stdex::extents<indx_type, D>, stdex::layout_left, stdex::default_accessor<Float>>;
 using Left1DCView     =  stdex::mdspan<const Float, stdex::extents<indx_type, D>, stdex::layout_left, stdex::default_accessor<const Float>>;
-
-using Strided1DView   =  stdex::mdspan<Float, stdex::extents<indx_type, D>, stdex::layout_stride>;
-using Strided1DCView  =  stdex::mdspan<const Float, stdex::extents<indx_type, D>, stdex::layout_stride>;
-
-// Dynamic objects views:
-using StridedDyn1DView  = stdex::mdspan<Float, stdex::extents<dyn_indx_type, stdex::dynamic_extent>, stdex::layout_stride>;
-using StridedDyn1DCView = stdex::mdspan<const Float, stdex::extents<dyn_indx_type, stdex::dynamic_extent>, stdex::layout_stride>;
-
-// Static mapping for strided objects
-using Static1DMap = stdex::layout_stride::mapping<stdex::extents<indx_type, D>>;
-using Static2DMap = stdex::layout_stride::mapping<stdex::extents<indx_type, D, D>>;
-
-// Dynamic mapping for strided objects:
-using Dyn1DMap    = stdex::layout_stride::mapping<stdex::extents<dyn_indx_type, stdex::dynamic_extent>>;
-
 
 //! Thread-local Matrix-Vector multiplication.
 template <typename out_vec_t, typename in_mat_t, typename in_vec_t,  bool is_scaled = false>
@@ -131,11 +141,11 @@ inline void MM(out_mat_t out, const in_mat1_t A, const in_mat2_t B)
 template<int d> 
 inline decltype(auto) 
 kalman_update(Left1DView &a, 
-	      StridedDyn1DView &vs_, 
+	      auto &vs_,
 	      Left2DView &t,  
 	      Left2DView &p, 
-	      StridedDyn1DView &Fs_, 
-	      const StridedDyn1DCView &ys_, 
+	      auto &Fs_, 
+	      const auto &ys_,
 	      const Left2DCView &rqr, 
 	      const Left1DView &z, 
 	      const Float mu, 
@@ -255,10 +265,10 @@ kalman_update(Left1DView &a,
 template<int d, bool conf_int> 
 inline void 
 kalman_forecast(Left1DView &a, 
-	        StridedDyn1DView &fc_, 
+		auto &fc_,
 	        Left2DView &t,  
 	        Left2DView &p,
-	        StridedDyn1DView &F_fc_,  
+		auto &F_fc_,
 	        const Left2DCView &rqr, 
 	        const Left1DCView &z, 
 	        const Float mu, 
@@ -345,6 +355,47 @@ void dispatch_kalman_kernel(auto&& kalman_alg, const int nseries){
   return;	
 }
 
+template <GenericContainerTp container_tp>
+class GenericContainerWrapper{
+  public:	
+    using data_tp = typename container_tp::value_type;	
+    //
+    container_tp data_container;	
+
+    GenericContainerWrapper(const size_t n) : data_container(n) { }
+
+    GenericContainerWrapper(const size_t n, std::default_random_engine &g, std::uniform_real_distribution<Float> &d) : data_container(n) {
+      for (auto &v : data_container) {
+        if constexpr (not std::is_arithmetic<decltype(v)>::value) {
+          for (auto &u : v) u = d(g);
+	}
+      }    	  
+    }
+
+    GenericContainerWrapper(const container_tp &src) : data_container(src)  {}
+    //
+    auto Get() { 
+      return std::make_shared<GenericContainerWrapper<std::span<data_tp>>>( std::span{data_container} ); 
+    }
+    //
+    template<int ...d>
+    auto Accessor(const size_t tid, const size_t batch_id,  std::array<int, sizeof...(d)> strides) {
+       using View = stdex::mdspan<const Float, stdex::extents<int, d...>, stdex::layout_stride, stdex::default_accessor<const Float>>;	    
+       using Map = stdex::layout_stride::mapping<stdex::extents<int, d...>>;
+       //
+       return View{ data_container[tid].data() + batch_id,
+                    Map(stdex::extents<int, d...>{}, strides) };
+    }
+
+    template<ArithmeticTp dataTp, bool dummy = false>
+    auto D1DynAccessor(const size_t idx, dyn_indx_type ext, std::array<dyn_indx_type, 1> strides) {
+       using Map    = stdex::layout_stride::mapping<stdex::extents<dyn_indx_type, stdex::dynamic_extent>>;	 
+       using View   = stdex::mdspan<dataTp, stdex::extents<dyn_indx_type, stdex::dynamic_extent>, stdex::layout_stride>;
+
+       return View{ dummy == false ? data_container[idx].data() : nullptr, Map(stdex::extents<dyn_indx_type, stdex::dynamic_extent>{ext}, strides) };
+    }    
+};
+
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     std::cout << "Usage: "<< argv[0] <<" <Series> <Repeat>\n" << std::endl;
@@ -364,50 +415,42 @@ int main(int argc, char* argv[]) {
   
   std::default_random_engine gen;
   std::uniform_real_distribution<Float> dist(0.0,1.0);
-  
-  std::vector<bFloatNN> RQR(bnseries);
-  for (auto &v : RQR) {
-    for (auto &u : v) 
-      u = dist(gen);
-  }
 
-  std::vector<bFloatNN> T(bnseries);
-  for (auto &v : T) {
-    for (auto &u : v) 
-      u = dist(gen);
-  }
+  auto _RQR = GenericContainerWrapper<std::vector<bFloatNN>>(bnseries, gen, dist);
+  auto RQR  = _RQR.Get();
 
-  std::vector<bFloatNN> P(bnseries);
-  for (auto &v : P) {
-    for (auto &u : v) 
-      u = dist(gen);
-  }
-  
-  std::vector<bFloatN> Z(bnseries);
-  for (auto &v : Z) {
-    for (auto &u : v) 
-      u = dist(gen);
-  }
-  
-  std::vector<bFloatN> alpha(bnseries);
-  for (auto &v : alpha) {
-    for (auto &u : v) 
-      u = dist(gen);
-  }
-  
-  std::vector<Float> ys(nseries * nobs);
-  for (auto &v : ys) v = dist(gen);
-  
-  std::vector<Float> mu(nseries);
-  for (auto &v : mu) v = dist(gen);
+  auto _T   = GenericContainerWrapper<std::vector<bFloatNN>>(bnseries, gen, dist);
+  auto T    = _T.Get();
 
-  std::vector<Float> vs(nseries * nobs);
-  std::vector<Float> Fs(nseries * nobs);
+  auto _P   = GenericContainerWrapper<std::vector<bFloatNN>>(bnseries, gen, dist);
+  auto P    = _P.Get();
+  
+  auto _Z   = GenericContainerWrapper<std::vector<bFloatN>>(bnseries, gen, dist);
+  auto Z    = _Z.Get();
+  
+  auto _alpha = GenericContainerWrapper<std::vector<bFloatN>>(bnseries, gen, dist);
+  auto alpha  = _alpha.Get();
+  
+  auto _ys = GenericContainerWrapper<std::vector<Float1>>(nseries*nobs, gen, dist);
+  auto ys  = _ys.Get();
+  
+  auto _mu = GenericContainerWrapper<std::vector<Float1>>(nseries, gen, dist);
+  auto mu  = _mu.Get();
+
+  auto _vs = GenericContainerWrapper<std::vector<Float1>>(nseries*nobs);
+  auto vs  = _vs.Get();  
+
+  auto _Fs = GenericContainerWrapper<std::vector<Float1>>(nseries*nobs);
+  auto Fs  = _Fs.Get();  
   //
-  std::vector<Float> sum_logFs(nseries);
+  auto _sum_logFs = GenericContainerWrapper<std::vector<Float1>>(nseries);
+  auto sum_logFs  = _sum_logFs.Get();
   //
-  std::vector<Float> fc(nseries * fc_steps);
-  std::vector<Float> F_fc(nseries * fc_steps);
+  auto _fc = GenericContainerWrapper<std::vector<Float1>>(nseries * fc_steps);
+  auto fc  = _fc.Get();  
+  //
+  auto _F_fc = GenericContainerWrapper<std::vector<Float1>>(nseries * fc_steps);
+  auto F_fc  = _F_fc.Get();
 
   /**
   * Kalman loop kernel. Each thread computes kalman filter for a single series
@@ -416,18 +459,18 @@ int main(int argc, char* argv[]) {
 
   for (int n_diff = 0; n_diff < D; n_diff++) {
 
-    auto kalman_alg  =  [=, ys    = ys.data(),
-                            T     = T.data(),
-                            Z     = Z.data(),
-                            RQR   = RQR.data(),
-                            P     = P.data(),
-                            alpha = alpha.data(),
-                            mu    = mu.data(),
-                            vs    = vs.data(),
-                            Fs    = Fs.data(),
-                            sum_logFs = sum_logFs.data(),
-                            fc    = fc.data(),
-                            F_fc  = F_fc.data()] (const auto idx) {
+    auto kalman_alg  =  [=, &ys    = *ys.get(),
+                            &T     = *T.get(),
+                            &Z     = *Z.get(),
+                            &RQR   = *RQR.get(),
+                            &P     = *P.get(),
+                            &alpha = *alpha.get(),
+                            &mu    = *mu.get(),
+                            &vs    = *vs.get(),
+                            &Fs    = *Fs.get(),
+                            &sum_logFs = *sum_logFs.get(),
+                            &fc    = *fc.get(),
+                            &F_fc  = *F_fc.get()] (const auto idx) {
                               
                               const auto tid      = idx / bSize;
                               const auto batch_id = idx % bSize;                               
@@ -449,17 +492,11 @@ int main(int argc, char* argv[]) {
                               Left1DView       a_{a.data()};//                              
 
                               // Load global mem into registers 
-                              auto RQR_ = Strided2DCView{ RQR[tid].data() + batch_id,
-                                                           Static2DMap(stdex::extents<indx_type, D, D>{}, 
-                                                                        std::array<indx_type, 2>{bSize, bSize*D}) };
+			      //
+                              auto RQR_ = RQR.Accessor<D, D>(tid, batch_id, {bSize, bSize*D});                                                                                         //
+                              auto T_   = T.Accessor<D, D>(tid, batch_id, {bSize, bSize*D});
                               //
-                              auto T_   = Strided2DCView{ T[tid].data() + batch_id,
-                                                           Static2DMap(stdex::extents<indx_type, D, D>{}, 
-                                                                        std::array<indx_type, 2>{bSize, bSize*D}) };
-                              //
-                              auto P_   = Strided2DCView{ P[tid].data() + batch_id,
-                                                           Static2DMap(stdex::extents<indx_type, D, D>{}, 
-                                                                        std::array<indx_type, 2>{bSize, bSize*D}) };                              
+                              auto P_   = P.Accessor<D, D>(tid, batch_id, {bSize, bSize*D});
 #pragma unroll    
                               for (int i = 0; i < D; i++) {
 #pragma unroll                              
@@ -470,47 +507,33 @@ int main(int argc, char* argv[]) {
                                 }
                               }
                               
-                              auto Z_    = Strided1DCView{ Z[tid].data() + batch_id,
-                                                              Static1DMap(stdex::extents<indx_type, D>{}, 
-                                                                             std::array<indx_type, 1>{bSize}) };
+                              auto Z_    = Z.Accessor<D>(tid, batch_id, {bSize});
                               //
-                              auto alpha_= Strided1DCView{ alpha[tid].data() + batch_id,
-                                                              Static1DMap(stdex::extents<indx_type, D>{}, 
-                                                                             std::array<indx_type, 1>{bSize}) };                              
+                              auto alpha_= alpha.Accessor<D>(tid, batch_id, {bSize});
 #pragma unroll    
                               for (int i = 0; i < D; i++) {;                               
                                 if (n_diff > 0) z_(i) = Z_(i);
                                 a_(i)                 = alpha_(i);
                               }
-
-                              auto ys_ = StridedDyn1DCView( ys + idx, 
-                                                               Dyn1DMap(stdex::extents<dyn_indx_type, stdex::dynamic_extent>{nobs}, 
-                                                                           std::array<dyn_indx_type, 1>{nseries}) );
-                                                                         
+			      auto ys_ = ys.D1DynAccessor<const Float>(idx, nobs, {nseries});
                               //
-                              auto vs_ = StridedDyn1DView{  vs + idx, 
-                                                               Dyn1DMap(stdex::extents<dyn_indx_type, stdex::dynamic_extent>{nobs}, 
-                                                                           std::array<dyn_indx_type, 1>{nseries}) };                               
+                              auto vs_ = vs.D1DynAccessor<Float>(idx, nobs, {nseries});
 
-                              auto Fs_ = StridedDyn1DView{  Fs + idx, 
-                                                               Dyn1DMap(stdex::extents<dyn_indx_type, stdex::dynamic_extent>{nobs}, 
-                                                                           std::array<dyn_indx_type, 1>{nseries}) };                              
+			      auto Fs_ = Fs.D1DynAccessor<Float>(idx, nobs, {nseries});
 
-                              Float mu_ = intercept ? mu[idx] : 0.0;
+                              Float mu_ = intercept ? mu.data_container[idx][0] : 0.0;//Float1
     
                               Float sum_logFs_ = kalman_update<D>(a_, vs_, t_, p_, Fs_, ys_, rqr_, z_, mu_, n_diff, nseries, nobs);
 
                               // Forecast
-                              auto fc_   = StridedDyn1DView{ fc_steps ? fc + idx : nullptr, 
-                                                                Dyn1DMap(stdex::extents<dyn_indx_type, stdex::dynamic_extent>{fc_steps}, 
-                                                                            std::array<dyn_indx_type, 1>{nseries}) };                              
-                              auto F_fc_ = StridedDyn1DView{ forecast ? F_fc + idx : nullptr, 
-                                                                Dyn1DMap(stdex::extents<dyn_indx_type, stdex::dynamic_extent>{fc_steps}, 
-                                                                            std::array<dyn_indx_type, 1>{nseries}) };                               
+			      constexpr bool is_empty = fc_steps ? false : true;                                              
+			      auto fc_ = fc.D1DynAccessor<Float, is_empty>(idx, fc_steps, {nseries}); 
+
+			      auto F_fc_ = F_fc.D1DynAccessor<Float, is_empty>(idx, fc_steps, {nseries});                                              
 
                               kalman_forecast<D, forecast>(a_, fc_, t_, p_, F_fc_, rqr_, z_, mu_, n_diff, nseries, fc_steps);
 
-                              sum_logFs[idx] = sum_logFs_;
+                              sum_logFs.data_container[idx][0] = sum_logFs_;
 
                            };
     
@@ -529,7 +552,7 @@ int main(int argc, char* argv[]) {
   double sum = 0.0;
   
   for (int i = 0; i < fc_steps * nseries - 1; i++)
-    sum += fabs((fabs(F_fc[i+1]) - fabs(F_fc[i]))) / (fabs(F_fc[i+1]) + fabs(F_fc[i]));
+    sum += fabs((fabs(F_fc->data_container[i+1][0]) - fabs(F_fc->data_container[i][0]))) / (fabs(F_fc->data_container[i+1][0]) + fabs(F_fc->data_container[i][0]));
     
   std::cout << "Checksum: " << sum << std::endl;
 
